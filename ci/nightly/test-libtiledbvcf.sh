c0ea9611e01292ae57cb446f268d3a0435a59047
John Blischak <jdblischak@gmail.com>
2023-09-21 13:39:19 -0400

    Specify full path to run executables on Windows

--- ci/nightly/test-libtiledbvcf.sh
+++ ci/nightly/test-libtiledbvcf.sh
@@ -5,8 +5,8 @@ set -ex
 # and build directory is build-libtiledbvcf
 
 make -j2 -C build-libtiledbvcf/libtiledbvcf tiledb_vcf_unit
-./build-libtiledbvcf/libtiledbvcf/test/tiledb_vcf_unit
+$GITHUB_WORKSPACE/build-libtiledbvcf/libtiledbvcf/test/tiledb_vcf_unit
 
 # cli tests (require bcftools)
 # USAGE: run-cli-tests.sh <build-dir> <inputs-dir>
-TileDB-VCF/libtiledbvcf/test/run-cli-tests.sh build-libtiledbvcf TileDB-VCF/libtiledbvcf/test/inputs
+$GITHUB_WORKSPACE/TileDB-VCF/libtiledbvcf/test/run-cli-tests.sh build-libtiledbvcf TileDB-VCF/libtiledbvcf/test/inputs
