3ccdb892fd3f04e1333d42a6f5578b2a5e0b682c
John Blischak <jdblischak@gmail.com>
2023-09-22 12:25:52 -0400

    Fix Windows-style env vars for Python build

--- ci/nightly/build-tiledbvcf-py.bat
+++ ci/nightly/build-tiledbvcf-py.bat
@@ -4,10 +4,14 @@ rem Build (and test) tiledbvcf-py assuming source code directory is
 rem .\TileDB-VCF\apis\python
 
 set PATH=%GITHUB_WORKSPACE%\install\bin;%PATH%
-echo "PATH: $PATH"
+echo "PATH: %PATH%"
 
 cd TileDB-VCF\apis\python
-python setup.py develop --libtiledbvcf=$GITHUB_WORKSPACE\install\
+python setup.py develop --libtiledbvcf=%GITHUB_WORKSPACE%\install\
+if %ERRORLEVEL% neq 0 exit 1
+
 python -c "import tiledbvcf; print(tiledbvcf.version)"
+if %ERRORLEVEL% neq 0 exit 1
 
 pytest
+if %ERRORLEVEL% neq 0 exit 1
